b50dd0dfc66c1f9a3c715954eee0935a5614f0f1
yoontaehoonDev <siryou10@gmail.com>
2021-02-24 18:13:25 +0900

    preparing for upgrade

--- app/src/main/java/com/yoon/pms/domain/Log.java
+++ app/src/main/java/com/yoon/pms/domain/Log.java
@@ -1,65 +1,66 @@
 package com.yoon.pms.domain;
 
 public class Log {
-	// 아이디 구분 로그인 시간, 
-	// 아이디 구분 로그아웃 시간, 접속 유지 시간
-	private String id;
-	private String loginTime;
-	private String logoutTime;
+  // 아이디 구분 로그인 시간, 
+  // 아이디 구분 로그아웃 시간, 접속 유지 시간
+  private String id;
+  private String loginTime;
+  private String logoutTime;
+  private int stayedTime;
 
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getLoginTime() {
-		return loginTime;
-	}
-	public void setLoginTime(String loginTime) {
-		this.loginTime = loginTime;
-	}
-	public String getLogoutTime() {
-		return logoutTime;
-	}
-	public void setLogoutTime(String logoutTime) {
-		this.logoutTime = logoutTime;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((loginTime == null) ? 0 : loginTime.hashCode());
-		result = prime * result + ((logoutTime == null) ? 0 : logoutTime.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Log other = (Log) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (loginTime == null) {
-			if (other.loginTime != null)
-				return false;
-		} else if (!loginTime.equals(other.loginTime))
-			return false;
-		if (logoutTime == null) {
-			if (other.logoutTime != null)
-				return false;
-		} else if (!logoutTime.equals(other.logoutTime))
-			return false;
-		return true;
-	}
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getLoginTime() {
+    return loginTime;
+  }
+  public void setLoginTime(String loginTime) {
+    this.loginTime = loginTime;
+  }
+  public String getLogoutTime() {
+    return logoutTime;
+  }
+  public void setLogoutTime(String logoutTime) {
+    this.logoutTime = logoutTime;
+  }
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((loginTime == null) ? 0 : loginTime.hashCode());
+    result = prime * result + ((logoutTime == null) ? 0 : logoutTime.hashCode());
+    return result;
+  }
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Log other = (Log) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (loginTime == null) {
+      if (other.loginTime != null)
+        return false;
+    } else if (!loginTime.equals(other.loginTime))
+      return false;
+    if (logoutTime == null) {
+      if (other.logoutTime != null)
+        return false;
+    } else if (!logoutTime.equals(other.logoutTime))
+      return false;
+    return true;
+  }
 
 
 }
