a82f120f58e6652f91352099f63e868826abf27a
yoontaehoonDev <siryou10@gmail.com>
2021-02-28 19:17:30 +0900

    ReturnHandler added

--- app/src/main/java/com/yoon/pms/handler/ReturnHandler.java
+++ app/src/main/java/com/yoon/pms/handler/ReturnHandler.java
@@ -1,5 +1,16 @@
 package com.yoon.pms.handler;
 
+import com.yoon.pms.App;
+
 public class ReturnHandler {
 
+	public void service() {
+
+		if(App.location == 1) {
+
+		}
+		else if(App.location == 2) {
+
+		}
+	}
 }
